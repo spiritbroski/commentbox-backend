fb866e0bfe5933e14ea0f401026b14fb66b628be
febriano <rinoakbr@gmail.com>
2021-08-28 21:22:43 +0700

    adding some condition to be meet so that user can insert and update comment

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ app.use(cors())
 const { Deta } = require('deta');
 const deta = Deta('c08piu78_wyraVGQooFYPtpARJhkqxykuz9nZSa2b'); 
 const db = deta.Base('simple_db'); 
-app.get('/all', async (req, res) => res.json((await db.fetch({},{limit:5}))))
+app.get('/all/:proposal_id', async (req, res) =>{
+    if(!req.params.proposal_id) return res.json({status:false,data:[]});
+    const proposalData=await db.fetch({proposal_id:req.params.proposal_id},{limit:5,last:req.query?.last?req.query?.last:null});
+    return res.json({status:true,data:proposalData});
+})
 app.post('/add',async (req, res) => {
     const {name,markdown,reply}=req.body
-    console.log(req.body)
-    const insertedUser = await db.put({
+    if(!name&&!markdown&&!reply) return res.json({status:false,data:[]})
+    const insertedComment = await db.put({
         name,
         markdown,
         reply
     })
-    res.status(201).json(req.body.name);
+    return res.status(201).json(insertedComment);
 })
 app.post('/update', (req, res) => res.send('Hello World!'))
 app.post('/delete', (req, res) => res.send('Hello World!'))
