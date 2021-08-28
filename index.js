8e943d0e16d75b3dce6c23d2d8a431087d465025
febriano <rinoakbr@gmail.com>
2021-08-28 16:59:29 +0700

    bismillah sukses

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // install express with `npm install express` 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors())
 const { Deta } = require('deta');
 const deta = Deta('c08piu78_wyraVGQooFYPtpARJhkqxykuz9nZSa2b'); 
 const db = deta.Base('simple_db'); 
@@ -10,11 +13,11 @@ app.post('/add',async (req, res) => {
     const {name,markdown,reply}=req.body
     console.log(req.body)
     const insertedUser = await db.put({
-        name:"dsads",
-        markdown:"das",
-        reply:[]
+        name,
+        markdown,
+        reply
     })
-    res.status(201).json(req.body);
+    res.status(201).json(req.body.name);
 })
 app.post('/update', (req, res) => res.send('Hello World!'))
 app.post('/delete', (req, res) => res.send('Hello World!'))
