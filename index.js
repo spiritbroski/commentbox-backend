93d96d0ace9899dcc003cf4dd1edfb32856007aa
febriano <rinoakbr@gmail.com>
2021-09-02 10:29:51 +0700

    logic for comment almost done just need auth and sign

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = deta.Base("simple_db");
 app.get("/all/:proposal_id", async (req, res) => {
   if (!req.params.proposal_id) return res.json({ status: false, data: [] });
   const proposalData = await db.fetch(
-    { proposal_id: req.params.proposal_id,main_thread:true },
+    { proposal_id: req.params.proposal_id, main_thread: true },
     { limit: 5, last: req.query.last ? req.query.last : null }
   );
   return res.json({ status: true, data: proposalData });
@@ -19,97 +19,132 @@ app.get("/all/:proposal_id", async (req, res) => {
 app.get("/all_reply/:proposal_id/:main_thread_id", async (req, res) => {
   if (!req.params.proposal_id) return res.json({ status: false, data: [] });
   const proposalData = await db.fetch(
-    { proposal_id: req.params.proposal_id,main_thread:false,main_thread_id:req.params.main_thread_id },
+    {
+      proposal_id: req.params.proposal_id,
+      main_thread: false,
+      main_thread_id: req.params.main_thread_id,
+    },
     { limit: 5, last: req.query.last ? req.query.last : null }
   );
   return res.json({ status: true, data: proposalData });
 });
 app.post("/add", async (req, res) => {
   const { author, markdown, proposal_id } = req.body;
-  if (!author || !markdown || !proposal_id )
+  if (!author || !markdown || !proposal_id)
     return res.json({ status: false, data: [] });
-  const insertedComment = await db.put({
-    author,
-    markdown,
-    proposal_id,
-    timestamp:new Date().getTime(),
-    main_thread:true
-  },new Date().getTime().toString());
-  if (insertedComment) return res.status(201).json({ status: true, data: [] });
+  const insertedComment = await db.put(
+    {
+      author,
+      markdown,
+      proposal_id,
+      timestamp: new Date().getTime(),
+      main_thread: true,
+    },
+    new Date().getTime().toString()
+  );
+  if (insertedComment) return res.status(201).json({ status: true, data: insertedComment });
   else return res.json({ status: false, data: [] });
 });
 app.post("/add_reply", async (req, res) => {
-  const { author, markdown, proposal_id,main_thread_id,reply_to,reply,reply_thread_id } = req.body;
-  if (!author || !markdown || !proposal_id || !main_thread_id || !reply_to || !reply || !reply_thread_id)
-    return res.json({ status: false, data: [] });
-  const insertedComment = await db.put({
+  const {
     author,
     markdown,
     proposal_id,
-    timestamp:new Date().getTime(),
-    main_thread:false,
     main_thread_id,
     reply_to,
     reply,
-    reply_thread_id
-  },new Date().getTime().toString());
-  if (insertedComment) return res.status(201).json({ status: true, data: [] });
+    reply_thread_id,
+  } = req.body;
+  if (
+    !author ||
+    !markdown ||
+    !proposal_id ||
+    !main_thread_id ||
+    !reply_to ||
+    !reply ||
+    !reply_thread_id
+  )
+    return res.json({ status: false, data: [] });
+  const insertedComment = await db.put(
+    {
+      author,
+      markdown,
+      proposal_id,
+      timestamp: new Date().getTime(),
+      main_thread: false,
+      main_thread_id,
+      reply_to,
+      reply,
+      reply_thread_id,
+    },
+    new Date().getTime().toString()
+  );
+  if (insertedComment) return res.status(201).json({ status: true, data: insertedComment });
   else return res.json({ status: false, data: [] });
 });
 app.post("/update/:key", async (req, res) => {
-  if(!req.params.key) return res.json({status:false});
-  try{
-    const update=req.body;
-    update.edit_timestamp=new Date().getTime();
-    await db.update(update,req.params.key)
+  if (!req.params.key) return res.json({ status: false });
+  try {
+    const update = req.body;
+    update.edit_timestamp = new Date().getTime();
+    await db.update(update, req.params.key);
     const getItemFirst = await db.get(req.params.key);
-    if(!getItemFirst.main_thread) {
-      let res = await db.fetch({reply_thread_id:getItemFirst.key});
-  let allItems = res.items;
-  while (res.last){
-    res = await db.fetch({reply_thread_id:getItemFirst.key}, {last: res.last});
-    allItems = allItems.concat(res.items);
-  }
-  for(let i=0;i<allItems.length;i++){
-    await db.update({deleted:false,edited:true},allItems[i].key)
-  }
+    if (!getItemFirst.main_thread) {
+      let res = await db.fetch({ reply_thread_id: getItemFirst.key });
+      let allItems = res.items;
+      while (res.last) {
+        res = await db.fetch(
+          { reply_thread_id: getItemFirst.key },
+          { last: res.last }
+        );
+        allItems = allItems.concat(res.items);
+      }
+      for (let i = 0; i < allItems.length; i++) {
+        await db.update({ deleted: false, edited: true }, allItems[i].key);
+      }
     }
-    return res.json({status:true})
-  }catch(e){
-    return res.json({status:false})
+    
+    return res.json({ status: true,data:getItemFirst });
+  } catch (e) {
+    return res.json({ status: false });
   }
 });
 app.delete("/delete/:key", async (req, res) => {
   if (!req.params.key) return res.json({ status: false });
   const getItemFirst = await db.get(req.params.key);
-  if(getItemFirst.main_thread) {
-    let res = await db.fetch({main_thread_id:getItemFirst.key});
-let allItems = res.items;
-while (res.last){
-  res = await db.fetch({main_thread_id:getItemFirst.key}, {last: res.last});
-  allItems = allItems.concat(res.items);
-}
-for(let i=0;i<allItems.length;i++){
-  await db.delete(allItems[i].key)
-}
-  }else{
-    let res = await db.fetch({reply_thread_id:getItemFirst.key});
+  if (getItemFirst.main_thread) {
+    let res = await db.fetch({ main_thread_id: getItemFirst.key });
     let allItems = res.items;
-    while (res.last){
-      res = await db.fetch({reply_thread_id:getItemFirst.key}, {last: res.last});
+    while (res.last) {
+      res = await db.fetch(
+        { main_thread_id: getItemFirst.key },
+        { last: res.last }
+      );
       allItems = allItems.concat(res.items);
     }
-    for(let i=0;i<allItems.length;i++){
-      await db.update({deleted:true,edited:false},allItems[i].key)
-    }   
+    for (let i = 0; i < allItems.length; i++) {
+      await db.delete(allItems[i].key);
+    }
+  } else {
+    let res = await db.fetch({ reply_thread_id: getItemFirst.key });
+    let allItems = res.items;
+    while (res.last) {
+      res = await db.fetch(
+        { reply_thread_id: getItemFirst.key },
+        { last: res.last }
+      );
+      allItems = allItems.concat(res.items);
+    }
+    for (let i = 0; i < allItems.length; i++) {
+      await db.update({ deleted: true, edited: false }, allItems[i].key);
+    }
   }
   await db.delete(req.params.key);
   const getItem = await db.get(req.params.key);
-  if (!getItem){
-    const update=await db.fetch()
+  if (!getItem) {
+    const update = await db.fetch();
     return res.status(201).json({ status: true, data: [] });
-  } 
-  else return res.json({ status: false, data: [] });
+  } else return res.json({ status: false, data: [] });
 });
 
 // export 'app'
